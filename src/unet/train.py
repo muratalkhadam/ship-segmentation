78a1e134a363875f5175e773b8b77bb30da141f6
muratalkhadam <muratalkhadam@gmail.com>
2023-06-25 14:50:35 +0300

    refactored train module

--- src/unet/train.py
+++ src/unet/train.py
@@ -1,16 +1,18 @@
 from .metrics import dice_loss, dice_coef
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.model_selection import train_test_split
+from datetime import datetime
 
 
-def train(model, X_train, Y_train, validation_size=0.2, epochs=30, batch_size=32):
+def train(model, X_train, Y_train, validation_size=0.2, epochs=30, batch_size=16, model_name='unet-segmentation'):
     X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=validation_size, random_state=42)
 
     model.compile(optimizer='adam', loss=[dice_loss], metrics=[dice_coef])
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     callbacks = [
-        ModelCheckpoint('unet-segmentation.h5', verbose=1, save_best_only=True),
-        EarlyStopping(monitor='dice_coef', patience=7, mode='max')
+        ModelCheckpoint(f'models/{model_name}{current_time}.h5', verbose=1, save_best_only=True),
+        EarlyStopping(monitor='val_dice_coef', patience=5, mode='max')
     ]
 
     return model.fit(X_train, Y_train, validation_data=(X_val, Y_val),
